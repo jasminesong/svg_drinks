7d96d875bee2933d31bdba06611f64242cbc5af3
Jasmine Song <Jasmine@Jasmines-MacBook-Pro.local>
2016-10-08 20:16:10 -0400

    put all glass svgs on page

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	// $('#glass01 #juice-left').addClass('juice_disappear');
+ //    $('#glass01 #juice-right').addClass('juice_disappear');
+ //    $('#glass01 #juice-light').addClass('juice_disappear');
+
+
+$("#glass01 #juice-left").attr("class", "juice_disappear");
+$("#glass01 #juice-right").attr("class", "juice_disappear");
+$("#glass01 #juice-light").attr("class", "juice_disappear");
+
+});
